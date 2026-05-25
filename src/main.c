ab7f99667b04b804a6ac6bb246e627981d6a38c6
andre-axt <andreaxtbachtold@gmail.com>
2026-05-25 16:17:01 -0300

    Add signal handling and clean up SDL resources

--- src/main.c
+++ src/main.c
@@ -1,16 +1,25 @@
 #include "chip8.h"
-#include <stdio.h>
 #include "graphic.h"
 #include <SDL2/SDL.h>
+#include <stdio.h>
+#include <signal.h>
 
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
+void stop(int sig){
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+	exit(0);
+}
+
 int main(){
+	signal(SIGINT, stop);
     Chip8 chip;
     ini_chip8(&chip);
     ini_SDL(&window, &renderer);
-    loader_rom(&chip, "../roms/clock.ch8");
+    loader_rom(&chip, "");
     int running = 1;
     SDL_Event event;
     const double timer_update = 1000 / 60;
@@ -18,30 +27,30 @@ int main(){
     int last_time = 0;
     double delta_time = 0;
     while (running){
-	while(SDL_PollEvent(&event)) {    
-        	if(event.type == SDL_QUIT){
-        		running = 0;
+		while(SDL_PollEvent(&event)) {    
+	        	if(event.type == SDL_QUIT){
+	        		running = 0;
+				}
 		}
-	}
-        
-	update_keys(&chip);
-        now = SDL_GetPerformanceCounter();
-        delta_time += (double)((now - last_time) * 1000 / (double)SDL_GetPerformanceFrequency());
-        last_time = now;
+	        
+		update_keys(&chip);
+		now = SDL_GetPerformanceCounter();
+		delta_time += (double)((now - last_time) * 1000 / (double)SDL_GetPerformanceFrequency());
+		last_time = now;
 
-        if (delta_time >= timer_update){
-            if(chip.delay_timer > 0){
-                chip.delay_timer--;
-            }
-            if(chip.sound_timer > 0){
-                chip.sound_timer--;
+		if (delta_time >= timer_update){
+			if(chip.delay_timer > 0){
+				chip.delay_timer--;
+			}
+			if(chip.sound_timer > 0){
+				chip.sound_timer--;
 
-            }
-            delta_time -= timer_update;
-        }
-        	
-        cycle_chip8(&chip, renderer);
-	SDL_Delay(2);
+			}
+			delta_time -= timer_update;
+		}
+			
+		cycle_chip8(&chip, renderer);
+		SDL_Delay(2);
     }
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
